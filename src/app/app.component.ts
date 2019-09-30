6250cca1c714a0b473cfcdfc05ff2b69e33ab61b
arnabbhat82 <arnabbhat@gmail.com>
2019-09-30 11:21:58 +0530

    assigning ngForm to reference #f for accessing the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven';
-  onSubmit() {
-    console.log('Submitted!');
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
